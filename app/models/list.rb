1857239e5225aab742351feeac6e8c8949d6addf
elliott fuller <efuller1101@gmail.com>
2021-11-20 15:32:00 +1100

    bookmark controller

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
-    has_many :movies, dependent: :destroy
-    has_many :bookmarks
+    has_many :bookmarks, dependent: :destroy
+    has_many :movies, through: :bookmarks
     validates :name, presence: true
     validates :name, uniqueness: { scope: :name, message: 'Name taken' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- resources :bookmarks
- resources :lists
+ resources :lists do
+  resources :bookmarks, only: [ :new, :create ]
+ end
+ resources :bookmarks, :destroy
  resources :movies
 end
