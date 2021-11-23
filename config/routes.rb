b125364bc64583f188c968510ff73d7e1268eb6f
elliott fuller <efuller1101@gmail.com>
2021-11-23 19:33:37 +1100

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- resources :lists do
-  resources :bookmarks, only: [ :new, :create ]
- end
- resources :bookmarks, :destroy
- resources :movies
+  root to: 'lists#index'
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
+  end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
