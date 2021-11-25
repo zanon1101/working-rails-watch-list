cb879935ccb7fc8d2fbe1fd28a16f49c9456102e
elliott fuller <efuller1101@gmail.com>
2021-11-25 20:16:25 +1100

    stuff

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,8 @@ class MovieController < ApplicationController
     def create
         @movie = Movie.new
     end
+
+    def article_params
+        params.require(:article).permit(:title, :body, :photo)
+      end
 end
